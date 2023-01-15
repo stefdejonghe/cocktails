8e48aaf1fd14f404447368be374de4e704b03fe2
De Jonghe Stef <61806955+stefdejonghe@users.noreply.github.com>
2023-01-15 18:30:53 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+        omnis repellendus illo impedit commodi numquam suscipit excepturi
+        dolorem neque repudiandae dolore. Incidunt necessitatibus modi
+        asperiores, veniam vero repellat! Officia cupiditate culpa accusantium
+        veritatis sit magni vero optio corporis, delectus rerum?
+      </p>
+    </section>
+  );
+};
+
+export default About;
